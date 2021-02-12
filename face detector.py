f4e4f95556b41cbd8cb52942453d43fa2794f35b
Samfxmoha <78876913+Samfxmoha@users.noreply.github.com>
2021-02-12 08:50:41 +0100

    Add files via upload

---
+++ face detector.py
@@ -0,0 +1,105 @@
+import cv2  
+import dlib
+import imutils    
+from imutils import face_utils
+
+import numpy as np
+
+face_detector=dlib.get_frontal_face_detector()
+landmark_detector=dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+
+from sklearn.externals import joblib
+
+clsfr=joblib.load('KNN_Model.sav')
+
+
+def predict_face_type(points):
+
+    label_dict={0:'Diamond',1:'Oblong',2:'Oval',3:'Round',4:'Square',5:'Triangle'}
+    
+    my_points=points[2:9,0]
+
+    D1=my_points[6]-my_points[0]
+    D2=my_points[6]-my_points[1]
+    D3=my_points[6]-my_points[2]
+    D4=my_points[6]-my_points[3]
+    D5=my_points[6]-my_points[4]
+    D6=my_points[6]-my_points[5]
+
+    d1=D2/float(D1)*100
+    d2=D3/float(D1)*100
+    d3=D4/float(D1)*100
+    d4=D5/float(D1)*100
+    d5=D6/float(D1)*100
+
+    result=clsfr.predict([[d1,d2,d3,d4,d5]])
+    label=result[0]
+    #print(label)
+    text=label_dict[label]
+    cv2.putText(img,text,(40,40),cv2.FONT_HERSHEY_SIMPLEX,1.3,(255,255,255),2)	
+
+    
+#camera=cv2.VideoCapture(0)
+#camera object has the access to the default camera of your pc
+
+while(True):
+
+    #ret,img=camera.read()
+    imagepath = input("input img : ")
+    img = cv2.imread(imagepath)
+    #img is a single frame (RGB) captured by the camera
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    #img will be converted into a gray image
+
+    rect=face_detector(gray)
+
+    (height,width)=img.shape[0:2]
+    img[0:50,0:width]=[0,255,0]
+    
+    try:
+
+        points=landmark_detector(gray,rect[0])
+        #getting the 68 points of the face
+        points=face_utils.shape_to_np(points)
+        #converting the points into a numpy array
+
+        count=1
+        for p in points:
+
+            cen=(p[0],p[1])
+            if count == 1:
+             xx=p[0]
+             yy=p[1]
+            cv2.circle(img,cen,2,(0,255,0),-1)
+            cv2.putText(img,str(count),cen,cv2.FONT_HERSHEY_SIMPLEX,0.3,(0,255,255),1)
+            count=count+1
+            
+        predict_face_type(points)
+        
+    except Exception as e:
+
+       print(e)
+
+    #cv2.imshow('LIVE',img)img1 = cv2.imread('105.jpg')
+    """img2 = cv2.imread('eyes.png')
+    # I want to put logo on top-left corner, So I create a ROI
+    rows,cols,channels = img2.shape
+    roi = img[0:rows, 0:cols]
+    print(xx)
+    print(yy)
+    # Now create a mask of logo and create its inverse mask also
+    img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2gray, 100, 255, cv2.THRESH_BINARY)
+    mask_inv = cv2.bitwise_not(mask)
+    # Now black-out the area of logo in ROI
+    img1_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+	# Take only region of logo from logo image.
+    img2_fg = cv2.bitwise_and(img2,img2,mask = mask)
+	# Put logo in ROI and modify the main image
+    dst = cv2.add(img1_bg,img2_fg)
+    img[0:rows, 0:cols ] = dst
+    cv2.imshow('res',img)
+    """
+    cv2.imshow('dst',img)
+    cv2.waitKey(1)
